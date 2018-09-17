051c74625e63238c60cf0b7c3c9a89ecfef21797
zettrix <fibreun@gmail.com>
2018-09-17 12:40:57 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var length = preferences.length,
+      count = 0,
+      k;
+
+      for( var i = 0; i < length; i++){
+        k = preferences[i] - 1;
+
+        var k2 = preferences[k] - 1;
+        if(k == k2) continue
+
+        var k3 = preferences[k2] - 1;
+        if(k2 == k3) continue
+
+        if(k3 == k){
+          count++;
+        }
+        return count / 3
+      }
+
 };
